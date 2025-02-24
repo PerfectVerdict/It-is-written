050e1d36c1bff26b2642d1e68460766a6c1ddd72
Ethan Metz <perfectverdict@protonmail.com>
2025-02-24 11:10:49 -0500

    mobile friendly

--- quotesv2/app/page.tsx
+++ quotesv2/app/page.tsx
@@ -4,25 +4,19 @@ import { useState } from "react";
 
 console.log(messages)
 export default function Home() {
-  //* Create a state variable for the quote
   const [quote, setQuote] = useState<string>("");
-  //* Function to change quote to a random one
   const getRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * messages.length);
     setQuote(messages[randomIndex]);
   }
   return (
     <>
-      {/* Render the quote dynamically */}
       <p className="quote">{quote}</p>
-
       <div className="buttonContainer" style={{
         display: "flex",
         marginTop: "20px",
-
       }}>
-        <button className="button" onClick={getRandomQuote}>It Is Written</button>
-
+        <button className="button" onClick={getRandomQuote} style={{ fontSize: "40px" }}>It Is Written</button>
       </div>
     </>
   );
